7c449c69adcde8957fe3f6e73b4d134250c1ce4a
Nandini Agarwal <104410750+itsnandini@users.noreply.github.com>
2022-07-23 15:53:22 +0530

    Create Reverse Linked List.py

---
+++ ALGO I/Day 10/Reverse Linked List.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        next = None
+        while head:
+            head.next, head, next = next, head.next, head
+        return next
