67e1bdd8ba1611d50020869175b114fdd85fdf89
Nandini Agarwal <104410750+itsnandini@users.noreply.github.com>
2022-07-31 16:36:10 +0530

    Create Increasing Triplet Subsequence.py

---
+++ DS II/Day 5/Increasing Triplet Subsequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        min_num, a, b = float("inf"), float("inf"), float("inf")
+        for c in nums:
+            if min_num >= c:
+                min_num = c
+            elif b >= c:
+                a, b = min_num, c
+            else:  # a < b < c
+                return True
+        return False
