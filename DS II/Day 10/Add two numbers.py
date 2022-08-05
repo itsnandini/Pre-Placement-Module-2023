b18318c1270e230864269e9544aa0e510d0ce91e
Nandini Agarwal <104410750+itsnandini@users.noreply.github.com>
2022-08-05 12:18:10 +0530

    Create Add two numbers.py

---
+++ DS II/Day 10/Add two numbers.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insert(self,l,v):
+        if l==None:
+            l.val=v
+        else:
+            r=l
+            while r.next!=None:
+                r=r.next
+            tmp=ListNode(v)
+            r.next=tmp
+    def addTwoNumbers(self, l1, l2):
+        res=ListNode(0)
+        s,s2='',''
+        while l1!=None:
+            s+=str(l1.val)
+            l1=l1.next
+        while l2!=None:
+            s2+=str(l2.val)
+            l2=l2.next
+        s=int(s[::-1])+int(s2[::-1])
+        for x in str(s)[::-1]:
+            self.insert(res,int(x))
+        return res.next
