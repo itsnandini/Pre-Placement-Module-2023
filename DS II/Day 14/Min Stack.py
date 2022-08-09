e6a84a75033f7c3f160a2aaf4ad3752bff84ccc4
Nandini Agarwal <104410750+itsnandini@users.noreply.github.com>
2022-08-09 09:32:14 +0530

    Create Min Stack.py

---
+++ DS II/Day 14/Min Stack.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.p = []
+        self.m = 9999999999999
+    def push(self, x: int) -> None:
+        self.p.append(x)
+        self.m=min(self.m,x)
+    def pop(self) -> None:
+        tem=self.p.pop()
+        if tem==self.m:
+            if self.p:
+                self.m=min(self.p)
+            else:
+                self.m=9999999999999
+    def top(self) -> int:
+        return self.p[-1]
+
+    def getMin(self) -> int:
+        if self.p:
+            return self.m
+        else:
+            return None
